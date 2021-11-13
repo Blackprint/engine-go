9814e8ca06a972b0bdc7aad45a7f18e4225e71a6
StefansArya <stefansarya1@gmail.com>
2021-11-13 21:20:58 +0700

    Fix node port upgrade

--- engine/interface.go
+++ engine/interface.go
@@ -1,6 +1,10 @@
 package engine
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
+
 	portType "github.com/blackprint/engine-go/port"
 	"github.com/blackprint/engine-go/types"
 	"github.com/blackprint/engine-go/utils"
@@ -10,7 +14,7 @@ var portList = [3]string{"Input", "Output", "Property"}
 
 type InterfaceData map[string]interface{}
 type Interface struct {
-	customEvent
+	*customEvent
 	QInitialized bool // for internal library only
 
 	Id    string
@@ -32,47 +36,69 @@ type Interface struct {
 // To be overriden
 func (iface *Interface) Init() {}
 
+var reflectKind = reflect.TypeOf(reflect.Int)
+
 // Private (to be called for internal library only)
 func (iface *Interface) QPrepare() {
 	node := iface.Node
+	fmt.Println(iface.Namespace)
+
+	if node == nil {
+		panic("Nani!?")
+	}
 
 	for i := 0; i < 3; i++ {
 		which := portList[i]
-		port := utils.GetProperty(node, which).(map[string]portType.PortFeature) // get value by property name
+		port := utils.GetProperty(node, "T"+which).(map[string]interface{}) // get value by property name
 
 		if port == nil {
 			continue
 		}
 
-		ifacePort := map[string]Port{}
+		ifacePort := map[string]*Port{}
 		utils.SetProperty(iface, which, ifacePort)
 
+		upgradePort := map[string]GetterSetter{}
+
 		// name: string, config: PortFeature
-		for name, config := range port {
+		for name, config_ := range port {
+			fmt.Println(name)
+			var config portType.PortFeature
+			var type_ reflect.Kind
+			var feature int
+
 			var def interface{}
+			if reflect.TypeOf(config_) == reflectKind {
+				type_ = config_.(reflect.Kind)
 
-			type_ := config.Type
-			feature := config.Feature
-
-			if feature == portType.TypeTrigger {
-				def = config.Func
-				type_ = types.Function
-			} else if feature == portType.TypeArrayOf {
-				// pass
-			} else if type_ == types.Int {
-				def = 0
-			} else if type_ == types.Bool {
-				def = false
-			} else if type_ == types.String {
-				def = ""
-			} else if type_ == types.Array {
-				def = [0]interface{}{} // ToDo: is this actually working?
-			} else if type_ == types.Any { // Any
-				// pass
-			} else if type_ == types.Function {
-				// pass
+				if type_ == types.Int {
+					def = 0
+				} else if type_ == types.Bool {
+					def = false
+				} else if type_ == types.String {
+					def = ""
+				} else if type_ == types.Array {
+					def = [0]interface{}{} // ToDo: is this actually working?
+				} else if type_ == types.Any { // Any
+					// pass
+				} else if type_ == types.Function {
+					// pass
+				} else {
+					panic(iface.Namespace + ": '" + name + "' Port type(" + type_.String() + ") for initialization was not recognized")
+				}
 			} else {
-				panic("Port type or feature for initialization was not recognized")
+				config = config_.(portType.PortFeature)
+				type_ = config.Type
+				feature = config.Feature
+
+				if feature == portType.TypeTrigger {
+					def = config.Func
+					type_ = types.Function
+				} else if feature == portType.TypeArrayOf {
+					// pass
+				} else {
+					panic(iface.Namespace + ": '" + name + "' Port feature(" + strconv.Itoa(feature) + ") for initialization was not recognized")
+				}
 			}
 
 			linkedPort := Port{
@@ -84,7 +110,9 @@ func (iface *Interface) QPrepare() {
 				Feature: feature,
 			}
 
-			utils.SetProperty(iface, which, linkedPort.CreateLinker())
+			upgradePort[name] = linkedPort.CreateLinker()
 		}
+
+		utils.SetProperty(node, which, upgradePort)
 	}
 }
