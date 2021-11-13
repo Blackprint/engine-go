0bc312422cec436510078e731cfce3707ddacba8
StefansArya <stefansarya1@gmail.com>
2021-11-13 17:03:08 +0700

    Add more utility to simplify for using reflect

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,12 @@ import (
 	"reflect"
 )
 
+var EmptyArgs = []reflect.Value{}
+
+func IsPointer(obj interface{}) bool {
+	return reflect.ValueOf(obj).Kind() == reflect.Ptr
+}
+
 // obj must be a pointer
 func SetProperty(obj interface{}, key string, value interface{}) {
 	reflect.ValueOf(obj).Elem().FieldByName(key).Set(reflect.ValueOf(value))
@@ -12,6 +18,16 @@ func SetProperty(obj interface{}, key string, value interface{}) {
 // obj must be a pointer
 func GetProperty(obj interface{}, key string) interface{} {
 	return reflect.ValueOf(obj).Elem().FieldByName(key).Interface()
+}
+
+// obj must be a pointer
+func GetFunction(obj interface{}, key string) interface{} {
+	return reflect.ValueOf(obj).MethodByName(key).Interface()
+}
+
+// obj must be a pointer
+func CallFunction(obj interface{}, key string, val []reflect.Value) interface{} {
+	return reflect.ValueOf(obj).MethodByName(key).Call(val)
 }
 
 // obj must be a pointer
