35698adc7b4e8001104df9680208a97948b7a8f6
StefansArya <stefansarya1@gmail.com>
2021-11-14 11:44:20 +0700

    Move `main.go` to `main_test.go` and update Readme

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
+<p align="center"><a href="#" target="_blank" rel="noopener noreferrer"><img width="150" src="https://user-images.githubusercontent.com/11073373/141421213-5decd773-a870-4324-8324-e175e83b0f55.png" alt="Blackprint"></a></p>
+
+<h1 align="center">Blackprint Engine for Golang</h1>
+<p align="center">Run exported Blackprint on Golang environment.</p>
+
+<p align="center">
+    <a href='https://github.com/Blackprint/Blackprint/blob/master/LICENSE'><img src='https://img.shields.io/badge/License-MIT-brightgreen.svg' height='20'></a>
+</p>
+
 ## Blackprint Engine for Golang
 Note:
 This engine is focused for easy to use API, currently some implementation still not efficient because Golang doesn't support:
-- Class/Object Oriented Programming (solution: functional programming and `map[string] func()` will be used)
 - Getter/setter (solution: use function as a getter/setter)
-- Optional parameter (this will impact the performance of current engine implementation)
+- Optional parameter (solution: use `...interface{}`)
 
-`interface{}` or `any` still being used massively to make Blackprint works without too much complexity in Golang, so please don't expect too high. Currently looking for contributor who can tidy up and make the implementation more efficient if possible.
+`interface{}` or `any` still being used massively to make Blackprint works without too much complexity in Golang, so please don't expect too high for the performance. Currently looking for contributor who can tidy up and make the implementation more efficient if possible, and without adding more complexity.
 
 Some implementation also use `reflect` for obtaining and setting value by using dynamic field name. It may be improved on the future, current implementation is focused to be similar with PHP and JS.
+
+## Documentation
+> Warning: This project haven't reach it stable version (semantic versioning at v1.0.0)<br>
+> But please try to use it and help the author for improving this project
+
+> Need help writing '-'
 
 ## License
 MIT
--- main_test.go
+++ main_test.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"strconv"
+	"testing"
+	"time"
 
 	"github.com/blackprint/engine-go/engine"
 	"github.com/blackprint/engine-go/example"
 )
 
-// Test
-func main() {
-	example.RegisterAll()
-	instance := engine.New()
+// for benchmarking
+var instance *engine.Instance
+var input *example.InputSimpleIFace
 
+func TestMain(m *testing.M) {
+	log.SetFlags(0)
+	// log.SetOutput(ioutil.Discard)
+
+	example.RegisterAll()
+
+	// === Import JSON after all nodes was registered ===
+	// You can import the JSON to Blackprint Sketch if you want to view the nodes visually
+	instance = engine.New()
 	json := `{"Example/Math/Random":[{"i":0,"x":298,"y":73,"output":{"Out":[{"i":2,"name":"A"}]}},{"i":1,"x":298,"y":239,"output":{"Out":[{"i":2,"name":"B"}]}}],"Example/Math/Multiply":[{"i":2,"x":525,"y":155,"output":{"Result":[{"i":3,"name":"Any"}]}}],"Example/Display/Logger":[{"i":3,"id":"myLogger","x":763,"y":169}],"Example/Button/Simple":[{"i":4,"id":"myButton","x":41,"y":59,"output":{"Clicked":[{"i":2,"name":"Exec"}]}}],"Example/Input/Simple":[{"i":5,"id":"myInput","x":38,"y":281,"data":{"value":"saved input"},"output":{"Changed":[{"i":1,"name":"Re-seed"}],"Value":[{"i":3,"name":"Any"}]}}]}`
 	instance.ImportJSON([]byte(json))
 
@@ -19,17 +30,27 @@ func main() {
 	// Anyway.. lets to run something :)
 	button := instance.Iface["myButton"].(*example.ButtonSimpleIFace)
 
-	fmt.Println("\n>> I'm clicking the button")
+	log.Println("\n>> I'm clicking the button")
 	button.Clicked(123)
 
 	logger := instance.Iface["myLogger"].(*example.LoggerIFace)
-	fmt.Println("\n>> I got the output value: " + logger.Log().(string))
+	log.Println("\n>> I got the output value: " + logger.Log().(string))
 
-	fmt.Println("\n>> I'm writing something to the input box")
-	input := instance.Iface["myInput"].(*example.InputSimpleIFace)
+	log.Println("\n>> I'm writing something to the input box")
+	input = instance.Iface["myInput"].(*example.InputSimpleIFace)
 	input.Data["value"]("hello wrold")
 
 	// you can also use getNodes if you haven't set the ID
 	myLogger := instance.GetNodes("Example/Display/Logger")[0].(*example.LoggerNode).Iface.(*example.LoggerIFace)
-	fmt.Println("\n>> I got the output value: " + myLogger.Log().(string))
+	log.Println("\n>> I got the output value: " + myLogger.Log().(string))
+}
+
+func BenchmarkInputBox(b *testing.B) {
+	if instance == nil {
+		TestMain(nil)
+	}
+
+	for i := 0; i < b.N; i++ {
+		input.Data["value"]("hello wrold" + strconv.Itoa(time.Now().Nanosecond()))
+	}
 }
