eab61547814463b22741dcb0c2134290bbbcf667
MastahSenpai <26342994+ii64@users.noreply.github.com>
2021-11-14 18:56:26 +0700

    impl: CLI app
    Signed-off-by: MastahSenpai <26342994+ii64@users.noreply.github.com>

---
+++ internal/cmd/root.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var (
+	RootCmd = &cobra.Command{
+		Use:   "engine-go",
+		Short: "engine-go for Blackprint",
+		Long:  "engine-go is Blackprint engine",
+	}
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/blackprint/engine-go/internal/cmd"
+)
+
+func main() {
+	cmd.RootCmd.Execute()
+}
